8068b04b2ce97410ff9197349b0165794c3d437a
Lorena <lorenacapraru@gmail.com>
2023-10-18 20:16:56 +0100

    Task 1 DONE
    The board is created with appropriate indexes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Board from "./components/Board";
 
 function App() {
   return (
     <div className="main">
       <h1 className="title">BattleShip Game</h1>
+      <Board />
     </div>
   );
 }
