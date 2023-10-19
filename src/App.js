1155cbf5c2d64cc55d4cca1b163b87304d444166
Lorena <lorenacapraru@gmail.com>
2023-10-19 10:53:36 +0100

    Two Boards for the game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
-import Board from "./components/Board";
+import Board from "./components/Board/Board";
+import Panel from "./components/Board/Panel/Panel";
 
 function App() {
+  const player1 = "You";
+  const player2 = "Computer";
   return (
     <div className="main">
       <h1 className="title">BattleShip Game</h1>
-      <Board />
+      <div className="boards">
+        <Board player={player1} />
+        <Panel />
+        <Board player={player2} />
+      </div>
     </div>
   );
 }
